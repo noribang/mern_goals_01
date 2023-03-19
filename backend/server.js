bb0dac4f0f3989e36581f9319b703cdb67bd167a
hoonkim <hoonk9387@gmail.com>
2023-03-19 14:04:14 -0500

    Added first api get request.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 8000
-
+// Create express instance.
 const app = express()
 
+/**CRUD**/
+/*READ*/
+app.get('/api/goals', (req, res) => {
+    res.send('Get goals...')
+})
+
+
+// Listen for requests.
 app.listen(port, () => console.log(`Server started on port ${port}`))
