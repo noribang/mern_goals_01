f9023bf64113c627123882a4a5b53f58617a5a4e
hoonkim <hoonk9387@gmail.com>
2023-03-25 20:02:30 -0500

    Added set, update and delete functions from controller into routes.

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -15,19 +15,24 @@ const {getGoals,
 router.get('/', getGoals)
 
 /*CREATE*/
-router.post('/', (req, res) => {
-    // res.send('Get goals...')
-    res.status(200).json({message: 'Set goals...'})
-})
+// router.post('/', (req, res) => {
+//     // res.send('Get goals...')
+//     res.status(200).json({message: 'Set goals...'})
+// })
+router.post('/', setGoal)
+
 /*UPDATE*/
-router.put('/:id', (req, res) => {
-    // res.send('Get goals...')
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-})
+// router.put('/:id', (req, res) => {
+//     // res.send('Get goals...')
+//     res.status(200).json({message: `Update goal ${req.params.id}`})
+// })
+router.put('/:id', updateGoal)
+
 /*DESTROY*/
-router.delete('/:id', (req, res) => {
-    // res.send('Get goals...')
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
+// router.delete('/:id', (req, res) => {
+//     // res.send('Get goals...')
+//     res.status(200).json({message: `Delete goal ${req.params.id}`})
+// })
+router.delete('/:id', deleteGoal)
 
 module.exports = router
