275d3c7b633939467cae3e4e74d0f42682b150a5
hoonkim <hoonk9387@gmail.com>
2023-03-25 19:50:29 -0500

    Added create, update and destroy functions in goalscontroller.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,6 +5,30 @@ const getGoals = (req, res) => {
     res.status(200).json({ message: 'Get goals...yyy' })
 }
 
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+    res.status(200).json({message: 'Set goal...'})
+}
+
+// @desc    Update goal
+// @route   UPDATE /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+}
+
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+}
+
 module.exports = {
     getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
 }
