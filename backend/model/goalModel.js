a525913e85e514a69fac832bda347e30e7a80c41
hoonkim <hoonk9387@gmail.com>
2023-03-29 20:57:01 -0500

    Exported mongoose.model goalSchema.

--- backend/model/goalModel.js
+++ backend/model/goalModel.js
@@ -2,13 +2,15 @@
 const mongoose = require('mongoose')
 // db schema.
 const goalSchema = mongoose.Schema(
-{
-    text: {
-        type: String,
-        required: [true, 'Please add a text value.']
+    {
+        text: {
+            type: String,
+            required: [true, 'Please add a text value.']
+        },
     },
-},
-{
-    timestamps: true,
-}
+    {
+        timestamps: true,
+    }
 )
+
+module.exports = mongoose.model('Goal', goalSchema)
